10f19337e4a5ac81a73191ffcfee5e432811becd
kartikey <kartikkey0414@gmail.com>
2025-03-27 18:28:58 +0530

    otp generation and business logic implemented

---
+++ src/controllers/otpController.js
@@ -0,0 +1,32 @@
+const client = require("../config/twilioConfig");
+const OTP = require("../models/otpModel");
+
+const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+const sendOTP = async (req, res) => {
+  try {
+    const { phoneNumber } = req.body;
+
+    if (!phoneNumber) {
+      return res.status(400).json({ message: "Phone number is required" });
+    }
+
+    const otp = generateOTP();
+    const otpData = new OTP({ phoneNumber, otp });
+
+    await otpData.save();
+
+    await client.messages.create({
+      body: `Your OTP for login verification is: ${otp}\nPlease enter this OTP to complete your authentication.\nThis OTP is valid for 5 minutes.\n\nBest,\nYour Name`,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: phoneNumber,
+    });
+
+    res.status(200).json({ message: "OTP sent successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to send OTP", error });
+  }
+};
+
+module.exports = { sendOTP };
