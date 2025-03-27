0fd9060851ee55a1b52728b07ef88d400ea61945
kartikey <kartikkey0414@gmail.com>
2025-03-27 17:58:52 +0530

    mongodb connection setup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
