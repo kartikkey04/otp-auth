bf9f66d031c1c0eaeca14c23ecf66fdc7b321e5b
kartikey <kartikkey0414@gmail.com>
2025-03-27 18:50:06 +0530

    middleware added

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ const connectDB = require("./config/db");
 const otpRoutes = require("./routes/otpRoutes");
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+
+app.use("/api", otpRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
